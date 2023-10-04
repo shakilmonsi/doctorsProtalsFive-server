8d591c86bbd42f9c48e7df19f81952c283a0aaf1
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-10-04 22:58:38 +0600

    dashboard dtata show koran o

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-const express =require('express')
+const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 // require('dotenv').config()
 
-const app = express ()
-const cors= require('cors')
+const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
 // middiewarer 
@@ -17,24 +17,26 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fm710lc.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 // dotenv crud mongodb example
- 
-async function run(){
-  try{
-const appointmentOptionCollextion = client.db('doctorprotailfive-main').collection('adersoptions')
-const bookingsCollextion = client.db('doctorprotailfive-main').collection('bookings')
 
-app.get('/appointmentoption', async (req,res)=>{
-  
+async function run() {
+    try {
+        const appointmentOptionCollextion = client.db('doctorprotailfive-main').collection('adersoptions')
+        const bookingsCollextion = client.db('doctorprotailfive-main').collection('bookings')
 
-  const date = req.query.date;
+        app.get('/appointmentoption', async (req, res) => {
+
+
+            // Use Aggregate to query multiple collection and then merge data
+
+            const date = req.query.date;
             const query = {};
             const options = await appointmentOptionCollextion.find(query).toArray();
 
@@ -46,33 +48,99 @@ app.get('/appointmentoption', async (req,res)=>{
             options.forEach(option => {
                 const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
                 const bookedSlots = optionBooked.map(book => book.slot);
-  console.log(date, option.naem, bookedSlots)
- })
-res.send(options)
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remainingSlots;
+                console.log(remainingSlots.length)
+            })
+            res.send(options);
+        });
 
-})
-// class -74-4 
-/*
-* api nameng canvention
- app.get('/bookings')
- app.get('/bookings/:id')
- app.post('bookings')
- app.post('/bookings/:id')
-app.patch('/bookings/:id')
-app.delete('/bookings/:id')
+        app.get('/v2/appointmentoption', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollextion.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+        // class -74-4 
+        /*
+        * api nameng canvention
+         app.get('/bookings')
+         app.get('/bookings/:id')
+         app.post('bookings')
+         app.post('/bookings/:id')
+        app.patch('/bookings/:id')
+        app.delete('/bookings/:id')
+        */
+       //class-75  
+       app.get('/bokings',async(req,res)=>{
+        const email=req.query.email;
+        console.log(email)
+    const query ={email:email}
+    const bookings= await bookingsCollextion.find(query).toArray();
+    res.send(bookings)
+    })
+        // app.post('/bokings',async (req,res)=>{
+        app.post('/bokings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
 
-*/
-app.post('/bokings',async (req,res)=>{
-  const booking =req.body 
-  console.log(booking)
-  const result= await bookingsCollextion.insertOne(booking)
-  res.send(result)
-})
+            const alreadyBooked = await bookingsCollextion.find(query).toArray();
 
-  }
-  finally{
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
 
-  }
+            const result = await bookingsCollextion.insertOne(booking);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
 
 }
 run().catch(console.log)
@@ -81,9 +149,9 @@ run().catch(console.log)
 
 
 
-app.get('/',(req,res)=>{
-        res.send('running')
+app.get('/', (req, res) => {
+    res.send('running')
 })
-app.listen(port,()=>{
-        console.log(`running${port}`)
+app.listen(port, () => {
+    console.log(`running${port}`)
 })
